adce844bd2ed2bfb73672adb0686f67264fb91ee
Ankur  Singh <42868436+ankur-26197@users.noreply.github.com>
2024-09-17 17:56:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('Machine Learning app')
 
 st.info('This app builds a ML model')
 
-with st.expander('Data')
+with st.expander('Data'):
   st.write('Raw data')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
