5c688a09cad40b05e071a2f9fe38decbc6e90d52
Ankur  Singh <42868436+ankur-26197@users.noreply.github.com>
2024-09-17 18:19:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,6 @@ with st.expander('Input features'):
 
 encode = ['island', 'sex']
 df_penguins = pd.get_dummies(input_penguins, prefix=encode)
-df_penguins
 
 X = df_penguins[1:]
 input_row = df_penguins[:1]
@@ -51,6 +50,17 @@ input_row = df_penguins[:1]
 target_mapper = {'Adelie': 0,
                  'Chinstrap': 1,
                  'Gentoo': 2}
+
+def target_encode(val):
+  return target_mapper(val)
+
+y = y_raw.apply(target_encode)
+
+with st.expander('Data preparatio'):
+  st.write('**Encoded X (input penguin)**')
+  input_row
+  st.write('**Encoded y**')
+  y
 
 
   
