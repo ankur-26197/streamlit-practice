8f8c5da4fcbceee58683c0588e274f91555cd532
Ankur  Singh <42868436+ankur-26197@users.noreply.github.com>
2024-09-17 18:01:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,8 @@ st.info('This app builds a ML model')
 
 with st.expander('Data'):
   st.write('Raw data')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
   
 with st.expander('Data visualisation'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
