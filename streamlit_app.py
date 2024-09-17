839678dc43d9f2778e7cb80fc983ca58c306c661
Ankur  Singh <42868436+ankur-26197@users.noreply.github.com>
2024-09-17 18:13:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,9 @@ with st.expander('Data'):
   st.write('Raw data')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
+
+X_raw = df.drop('species', axis=1)
+y_raw = df.species
   
 with st.expander('Data visualisation'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
@@ -21,3 +24,32 @@ with st.sidebar:
   flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
   gender = st.selectbox('Gender', ('male', 'female'))
+
+  data = {'island': island,
+         'bill_length_mm': bill_length_mm,
+         'bill_depth_mm': bill_depth_mm,
+         'flipper_length_mm': flipper_length_mm,
+         'body_mass_g': body_mass_g,
+         'gender': gender}
+
+  input_df = pd.DataFrame(data, index=[0])
+  input_penguins = pd.concat([input_df, X_raw], axis=0)
+
+with st.expander('Input features'):
+  st.write('Input penguin')
+  input_df
+  st.write('**Combined penguins data**')
+  input_penguins
+
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+
+X = df_penguins[1:]
+input_row = df_penguins[:1]
+
+target_mapper = {'Adelie': 0,
+                 'Chinstrap': 1,
+                 'Gentoo': 2}
+
+
+  
